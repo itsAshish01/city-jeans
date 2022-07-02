0cc05fcc49561914e29bbb96f85b94c059977a53
Ashish Sharma <developer.ashish18@gmail.com>
2022-07-02 23:45:40 +0530

    Fix Navbar spacing.

--- components/Navbar.js
+++ components/Navbar.js
@@ -60,7 +60,7 @@ export default function Navbar() {
               <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
                 <button
                   type='button'
-                  className='bg-slate-800 p-1 rounded-full text-slate-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-800 focus:ring-white'
+                  className='bg-slate-800 p-1 rounded-full text-slate-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-800 focus:ring-white mr-4 lg:mr-7'
                 >
                   <span className='sr-only'>View notifications</span>
                   <ShoppingBagIcon className='h-6 w-6' aria-hidden='true' />
