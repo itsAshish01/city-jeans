198f013e9dba96e48df4f4c5e9610529180532c2
Ashish Sharma <developer.ashish18@gmail.com>
2022-07-02 20:47:07 +0530

    Update categories images.

--- sections/Categories.js
+++ sections/Categories.js
@@ -17,7 +17,7 @@ export default function Categories() {
         <div className='mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8'>
           <div className='group aspect-w-2 aspect-h-1 rounded-lg overflow-hidden sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2'>
             <img
-              src='https://tailwindui.com/img/ecommerce-images/home-page-03-featured-category.jpg'
+              src='https://res.cloudinary.com/ashishimages/image/upload/v1656774891/city-jeans/3_lvm3mb.png'
               alt="Two models wearing women's black cotton crewneck tee and off-white cotton crewneck tee."
               className='object-center object-cover group-hover:opacity-75'
             />
@@ -30,7 +30,7 @@ export default function Categories() {
                 <h3 className='font-semibold text-white'>
                   <a href='#'>
                     <span className='absolute inset-0' />
-                    New Arrivals
+                    Casuals
                   </a>
                 </h3>
                 <p aria-hidden='true' className='mt-1 text-sm text-white'>
@@ -41,7 +41,7 @@ export default function Categories() {
           </div>
           <div className='group aspect-w-2 aspect-h-1 rounded-lg overflow-hidden sm:relative sm:aspect-none sm:h-full'>
             <img
-              src='https://tailwindui.com/img/ecommerce-images/home-page-03-category-01.jpg'
+              src='https://res.cloudinary.com/ashishimages/image/upload/v1656774605/city-jeans/2_gykdfz.png'
               alt='Wooden shelf with slate and olive drab green baseball caps, next to wooden clothes hanger with sweaters.'
               className='object-center object-cover group-hover:opacity-75 sm:absolute sm:inset-0 sm:w-full sm:h-full'
             />
@@ -54,7 +54,7 @@ export default function Categories() {
                 <h3 className='font-semibold text-white'>
                   <a href='#'>
                     <span className='absolute inset-0' />
-                    Accessories
+                    Formals
                   </a>
                 </h3>
                 <p aria-hidden='true' className='mt-1 text-sm text-white'>
@@ -65,7 +65,7 @@ export default function Categories() {
           </div>
           <div className='group aspect-w-2 aspect-h-1 rounded-lg overflow-hidden sm:relative sm:aspect-none sm:h-full'>
             <img
-              src='https://tailwindui.com/img/ecommerce-images/home-page-03-category-02.jpg'
+              src='https://res.cloudinary.com/ashishimages/image/upload/v1656774605/city-jeans/1_mpxlvo.png'
               alt='Walnut desk organizer set with white modular trays, next to porcelain mug on wooden desk.'
               className='object-center object-cover group-hover:opacity-75 sm:absolute sm:inset-0 sm:w-full sm:h-full'
             />
@@ -78,7 +78,7 @@ export default function Categories() {
                 <h3 className='font-semibold text-white'>
                   <a href='#'>
                     <span className='absolute inset-0' />
-                    Workspace
+                    Ethinic
                   </a>
                 </h3>
                 <p aria-hidden='true' className='mt-1 text-sm text-white'>
