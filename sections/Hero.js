133e1b3c9679e3d100d32f76f37f532b66ec36bc
Ashish Sharma <developer.ashish18@gmail.com>
2022-07-02 23:54:58 +0530

    Fix profile dropdown z-index issue.

--- sections/Hero.js
+++ sections/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <header>
       <Navbar />
-      <div className='relative bg-white overflow-hidden -z-10'>
+      <div className='relative bg-white overflow-hidden'>
         <div className='pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48'>
           <div className='relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static'>
             <div className='sm:max-w-lg'>
