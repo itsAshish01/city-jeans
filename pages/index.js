1df78117fe4465b6dc1619b68ea31c0c2c3ae77a
Ashish Sharma <developer.ashish18@gmail.com>
2022-07-02 21:16:17 +0530

    Build the feature section.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Hero from '../sections/Hero';
 import Categories from '../sections/Categories';
 import Story from '../sections/Story';
+import Feature from '../sections/Feature';
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <Hero />
         <Categories />
         <Story />
+        <Feature />
       </main>
     </div>
   );
--- sections/Story.js
+++ sections/Story.js
@@ -1,6 +1,6 @@
-export default function Example() {
+export default function Story() {
   return (
-    <div className='relative bg-gray-800 py-32 px-6 sm:py-40 sm:px-12 lg:px-16'>
+    <div className='relative bg-slate-800 py-32 px-6 sm:py-40 sm:px-12 lg:px-16'>
       <div className='absolute inset-0 overflow-hidden'>
         <img
           src='https://tailwindui.com/img/ecommerce-images/home-page-03-feature-section-full-width.jpg'
@@ -10,7 +10,7 @@ export default function Example() {
       </div>
       <div
         aria-hidden='true'
-        className='absolute inset-0 bg-gray-900 bg-opacity-50'
+        className='absolute inset-0 bg-slate-900 bg-opacity-50'
       />
       <div className='relative max-w-3xl mx-auto flex flex-col items-center text-center'>
         <h2 className='text-3xl font-semibold tracking-tight text-white sm:text-4xl'>
@@ -23,7 +23,7 @@ export default function Example() {
         </p>
         <a
           href='#'
-          className='mt-8 w-full block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto'
+          className='mt-8 w-full block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-slate-900 hover:bg-slate-100 sm:w-auto'
         >
           Read our story
         </a>
