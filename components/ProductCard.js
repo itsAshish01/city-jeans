e42ea3905ca880c35005c48977570751cc291642
Ashish Sharma <developer.ashish18@gmail.com>
2022-07-06 10:32:25 +0530

    Added thumbnails in products.

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const ProductCard = ({ product }) => {
+  console.log(product);
   return (
     <div className='group relative'>
       <div className='w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none'>
         <img
-          src={product.imageSrc}
+          src={product.thumbnail.data.attributes.formats.medium.url}
           alt={product.imageAlt}
-          className='w-full h-full object-center object-cover lg:w-full lg:h-full'
+          className='w-full h-full object-top object-cover lg:w-full lg:h-full'
         />
       </div>
       <div className='mt-4 flex justify-between'>
@@ -18,7 +19,9 @@ const ProductCard = ({ product }) => {
               {product.name}
             </a>
           </h3>
-          <p className='mt-1 text-sm text-gray-500'>{product.color}</p>
+          <p className='mt-1 text-sm text-gray-500'>
+            {product.shortDescription}
+          </p>
         </div>
         <p className='text-sm font-medium text-gray-900'>{product.price}</p>
       </div>
--- pages/collection/index.js
+++ pages/collection/index.js
@@ -6,39 +6,29 @@ import Spinner from '../../components/Spinner';
 
 import { COLLECTION_QUERY } from '../../lib/query';
 
-const products = [
-  {
-    id: 1,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  // More products...
-];
-
 const Collection = () => {
   const [result] = useQuery({ query: COLLECTION_QUERY });
   const { fetching, error, data } = result;
 
   if (fetching) return <Spinner />;
   if (error) return <Error message={error.message} />;
-  console.log(data);
+
+  const products = data.collections.data;
 
   return (
     <div className='bg-white'>
       <Navbar />
       <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
-        <h2 className='text-2xl font-extrabold tracking-tight text-gray-900'>
-          Customers also purchased
+        <h2 className='text-2xl font-semibold tracking-tight text-gray-900'>
+          The City Jeans Collection
         </h2>
 
         <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
           {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
+            <ProductCard
+              key={product.attributes.slug}
+              product={product.attributes}
+            />
           ))}
         </div>
       </div>
